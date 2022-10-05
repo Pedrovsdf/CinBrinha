ccb87e1ae1f2876b8b98f0cc43680bc451e3b5a5
Pedrovsdf <59745153+Pedrovsdf@users.noreply.github.com>
2022-10-05 15:01:26 -0300

    Add files via upload

---
+++ Comida.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+import random
+import Variaveis as v
+
+# Classe da comida padrão
+class Comida(object):
+    # Definindo as informações principais da comida
+    def __init__(self):
+        self.posicao = (0, 0)
+        self.color = v.vermelho
+        self.aleatorizar_posicao()
+
+    # Aleatorizar a posição da comida
+    def aleatorizar_posicao(self):
+        self.posicao = (random.randint(0, int(v.largura_rede) - 1) * v.tamanho_rede,
+                        random.randint(0, int(v.altura_rede) - 1) * v.tamanho_rede)
+
+    # Desenhar a comida
+    def desenhar(self, superficie):
+        retangulo = pygame.Rect((self.posicao[0], self.posicao[1]), (v.tamanho_rede, v.tamanho_rede))
+        pygame.draw.rect(superficie, self.color, retangulo)
+        pygame.draw.rect(superficie, v.preto, retangulo, 1)
---
+++ Variaveis.py
@@ -0,0 +1,23 @@
+# Na tela, 480/480 é o canto superior esquerdo e 0/0 é o inferior direito
+# Largura da tela
+largura = 480
+# Altura da tela
+altura = 480
+# Definindo cores rgb
+preto = (0, 0, 0)
+branco = (255, 255, 255)
+cinza = (170, 170, 170)
+vermelho = (200, 40, 40)
+verde = (20, 200, 50)
+amarelo = (243, 213, 0)
+# Tamanho da rede/cada retângulo
+tamanho_rede = 20
+# Largura de cada retângulo
+largura_rede = largura / tamanho_rede
+# Altura de cada retângulo
+altura_rede = altura / tamanho_rede
+# Posições em que a cobra pode se mover
+CIMA = (0, -1)
+BAIXO = (0, 1)
+ESQUERDA = (-1, 0)
+DIREITA = (1, 0)
