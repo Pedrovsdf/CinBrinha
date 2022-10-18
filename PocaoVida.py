ecc226154fc60c3e897f8c3b219e6fca79835279
anasosl <108680344+anasosl@users.noreply.github.com>
2022-10-17 22:25:59 -0300

    não renasce em cima do outro

--- PocaoVida.py
+++ PocaoVida.py
@@ -1,20 +1,23 @@
 import pygame
 import random
 import Variaveis as v
+from Cobra import Cobra
 
 # Classe da vida
 class Vida(object):
     # Definindo as informações principais da comida
-    def __init__(self):
+    def __init__(self, lista_posicoes, posicao_cobra):
         self.posicao = (0, 0)
         self.color = v.rosa
-        self.aleatorizar_posicao()
+        self.aleatorizar_posicao(lista_posicoes, posicao_cobra)
 
 
     # Aleatorizar a posição da comida
-    def aleatorizar_posicao(self):
-        self.posicao = (random.randint(0, int(v.largura_rede) - 1) * v.tamanho_rede,
-                        random.randint(0, int(v.altura_rede) - 1) * v.tamanho_rede)
+    def aleatorizar_posicao(self, lista_posicoes, posicao_cobra):
+        self.posicao = ((random.randint(0, int(v.largura_rede) - 1) * v.tamanho_rede),
+                        (random.randint(0, int(v.altura_rede) - 1) * v.tamanho_rede))
+        if self.posicao in lista_posicoes or self.posicao in posicao_cobra:
+            self.aleatorizar_posicao(lista_posicoes, posicao_cobra)
 
     # Desenhar a comida
     def desenhar(self, superficie):
